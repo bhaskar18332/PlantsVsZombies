b15e2004e00caf2748af5d696503dcdbb61b4a2a
bhaskar18332 <55093002+bhaskar18332@users.noreply.github.com>
2019-10-30 15:27:03 +0530

    First commit

---
+++ New_GameController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap_project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author lenovo
+ */
+public class New_GameController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    private void New(ActionEvent e)
+    {
+       System.out.println("Done");
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
